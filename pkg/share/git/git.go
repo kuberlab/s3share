5dfa5aa693feccd6136827a6995326e988b03c96
Alexander Gunin <guninalexander@gmail.com>
2017-09-04 13:40:07 +0300

    use tmpfs

--- pkg/share/git/git.go
+++ pkg/share/git/git.go
@@ -3,10 +3,7 @@ package git
 import (
 	"fmt"
 	"github.com/dreyk/s3share/pkg/util"
-	"github.com/pborman/uuid"
 	"log/syslog"
-	"os"
-	"path/filepath"
 	"syscall"
 )
 
@@ -26,24 +23,17 @@ func (m *GitFSMount) Mount(path string) error {
 	} else if isMounted {
 		return nil
 	}
-	url := m.conf["url"].(string)
-	rootMnt := filepath.Join(os.TempDir(), uuid.New())
-	m.slog.Info(fmt.Sprintf("Clone %s to %s", url, rootMnt))
-	err := os.MkdirAll(rootMnt, os.ModePerm)
+	out, err := util.ExecCommand(m.exec, "mount", []string{"-t","tmpfs","tmpfs", path}, "")
 	if err != nil {
-		return fmt.Errorf("Failed create tmp dir for clonning repo: %v", err)
+		return fmt.Errorf("Failed mount tmpfs out='%v' error='%v'", string(out), err)
 	}
-	out, err := util.ExecCommand(m.exec, "git", []string{"clone", url, rootMnt}, rootMnt)
+	url := m.conf["url"].(string)
+	out, err = util.ExecCommand(m.exec, "git", []string{"clone", url, path}, path)
 	if err != nil {
 		return fmt.Errorf("Failed clone repo out='%v' error='%v'", string(out), err)
 	}
-	m.slog.Info(fmt.Sprintf("Mount %s to %s", rootMnt, path))
-	out, err = util.ExecCommand(m.exec, "mount", []string{"--bind", rootMnt, path}, "")
-	if err != nil {
-		return fmt.Errorf("Failed bind dirs out='%v' error='%v'", string(out), err)
-	}
 	if isMounted, err := util.IsMounted(path); err != nil {
-		m.slog.Warning("Can't et mount status: " + err.Error())
+		m.slog.Warning("Can't get mount status: " + err.Error())
 	} else {
 		m.slog.Info(fmt.Sprintf("Mount result is %v", isMounted))
 	}
@@ -56,23 +46,5 @@ func (m *GitFSMount) UnMount(path string) error {
 	} else if !isMounted {
 		return nil
 	}
-	out, err := util.ExecCommand(m.exec, "findmnt", []string{"-n", path}, "")
-	if err != nil {
-		return fmt.Errorf("Failed find mount point", string(out), err)
-	}
-	err = syscall.Unmount(path, 0)
-	if err != nil {
-		return err
-	}
-	_, p2, err := util.ParseFindMntOut(string(out))
-	if err != nil {
-		m.slog.Warning(fmt.Sprintf("Can't dinf mount source in %s", out))
-	} else {
-		m.slog.Info(fmt.Sprintf("Clear mout source %s", p2))
-		err := os.RemoveAll(p2)
-		if err != nil {
-			m.slog.Warning(fmt.Sprintf("Failed clear mount source %s: %v", p2, err))
-		}
-	}
-	return nil
+	return syscall.Unmount(path, 0)
 }
