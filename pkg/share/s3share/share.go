60dc1705bef7133266117f5331f299b695b72f4f
Alexander Gunin <guninalexander@gmail.com>
2017-09-11 12:48:13 +0300

    fix

--- pkg/share/s3share/share.go
+++ pkg/share/s3share/share.go
@@ -2,10 +2,10 @@ package s3share
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	//"github.com/aws/aws-sdk-go/aws"
+	//"github.com/aws/aws-sdk-go/aws/credentials"
+	//"github.com/aws/aws-sdk-go/aws/session"
+	//"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/dreyk/s3share/pkg/util"
 	"log/syslog"
 	"strings"
